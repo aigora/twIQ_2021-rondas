2219b3e6b7b6f323c5a012ae93205a7710e1c856
MrBotKa <61472188+MrBotKa@users.noreply.github.com>
2021-05-27 17:56:08 +0200

    Update Evalua_Instrumental.c

--- Evalua_Instrumental.c
+++ Evalua_Instrumental.c
@@ -219,7 +219,7 @@ void comenzar_examen_por_temas(Pregunta* preguntas){
 	system("cls");
 	
 	printf("%s, tu nota es ",p[0].nombre);
-	printf("i/10\n",puntuacion);
+	printf("%i/10\n",puntuacion);
 	printf("Pulse una tecla para continuar");
 	
 	getch();
